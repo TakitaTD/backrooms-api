4655c64edb43fe93b605fe9632fe6b57c4ceaccf
Timothy Takita <takitatd@gmail.com>
2022-06-06 19:46:53 +1000

    current capabilities: able to parse images, description (kinda), and entrances & exits. This is the worst, steaming pile of shit I've ever written. Honestly. This hurts.

---
+++ index.mjs
@@ -0,0 +1,99 @@
+import fetch from "node-fetch";
+import { JSDOM } from "jsdom";
+import fs from "fs";
+import { minify } from "html-minifier";
+
+const MAX = 120;
+let i = 114;
+/* https://stackoverflow.com/questions/48608119/javascript-remove-all-occurrences-of-a-value-from-an-array */
+function removeItem(array, item) {
+  var i = array.length;
+
+  while (i--) {
+    if (array[i] === item) {
+      array.splice(array.indexOf(item), 1);
+    }
+  }
+}
+
+(async () => {
+  if (process.argv[2]) {
+    if (process.argv[2] == "--clean") {
+      for (let i = 0; i <= MAX; i++) {
+        fs.unlinkSync("assets/level-" + i + ".json");
+      }
+      return;
+    }
+  }
+
+  // for (let i = 0; i <= MAX; i++) {
+  if (fs.existsSync("assets/level-" + i + ".json")) {
+    console.log("assets/level-" + i + ".json already exists. Skipping...");
+    // continue;
+  } else {
+    const response = await fetch(
+      "http://backrooms-wiki.wikidot.com/level-" + i
+    );
+    const data = await response.text();
+    let dom;
+    dom = new JSDOM(data);
+    console.clear();
+
+    let elements = Array.from(
+      dom.window.document.querySelector("#page-content").childNodes
+    );
+
+    const Exits = dom.window.document
+      .evaluate(
+        "//span[text()='Exits']",
+        dom.window.document,
+        null,
+        dom.window.XPathResult.ANY_TYPE,
+        null
+      )
+      .iterateNext()
+      .parentElement.nextElementSibling.textContent.split("\n");
+
+    removeItem(Exits, "");
+
+    console.log(Exits);
+
+    const levelData = {
+      title: dom.window.document
+        .querySelector("#page-title")
+        .textContent.toString()
+        .replaceAll("\n", "")
+        .replaceAll("  ", ""),
+      description: dom.window.document
+        .querySelector("#page-content")
+        .textContent.replaceAll(
+          "\n",
+          `
+`
+        ),
+      images: [],
+      exits: Exits,
+      entrances: [],
+    };
+    dom.window.document
+      .querySelector("#page-content")
+      .querySelectorAll("img")
+      .forEach((img) => {
+        img.removeAttribute("style");
+        levelData.images.push({
+          src: img.getAttribute("src"),
+          alt: img.getAttribute("alt"),
+        });
+        img.remove();
+      });
+    console.log(levelData.title);
+    fs.writeFile(
+      "assets/level-" + i + ".json",
+      JSON.stringify(levelData),
+      (err) => {
+        if (err) return console.log("ERROR: " + err);
+      }
+    );
+  }
+  // }
+})();
